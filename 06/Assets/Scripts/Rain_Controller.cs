f2b93eafbf0fd569e6ad2ceb21052fa3bd70e9cd
PARK SEONG HYEON <lovehyun95@gmail.com>
2023-08-09 15:58:58 +0900

    임시

--- 06/Assets/Scripts/Rain_Controller.cs
+++ 06/Assets/Scripts/Rain_Controller.cs
@@ -10,13 +10,12 @@ public class Rain_Controller : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
 
-    void 
+    }
+}
